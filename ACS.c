2e22756d23dfbfb91bd7fbeeeb560d90908a5b5a
Parampreet Singh <params@uvic.ca>
2024-06-30 02:41:59 -0700

    new

--- ACS.c
+++ ACS.c
@@ -104,6 +104,9 @@ int main(int argc, char *argv[]){
     for(int k = 0; k < size; k++){
         pthread_join(customerThreads[k], NULL); //Join the customer threads
     }
+    for(int l = 0; l < CLERKS; l++){
+        pthread_join(clerkThreads[l], NULL); //Join the clerk threads
+    }
 
     pthread_mutex_destroy(&businessQueueMutex); //Destroy the business queue mutex
     pthread_mutex_destroy(&economyQueueMutex); //Destroy the economy queue mutex
@@ -191,7 +194,7 @@ void* clerkThread(void* param) {
     int clerk_id = *((int*)param);
     free(param);
     usleep(100000); 
-    while (TRUE) {
+    while (1) {
         struct Customer customer;
         int isBusinessCustomer = 0;
 
